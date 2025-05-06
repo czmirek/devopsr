f1aee9baa56098d59a169c2e56c380e205f33675
Miroslav Thompson <miroslav.thompson@projectport.cz>
2025-05-06 20:21:38 +0200

    wip

--- src/Devopsr.Web/Program.cs
+++ src/Devopsr.Web/Program.cs
@@ -1,6 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();
